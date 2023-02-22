e8013d224fda504a2915cb9e80cc1fe3cf766372
MohammadZiaQaderi <116964448+MohammadZiaQaderi@users.noreply.github.com>
2023-02-22 03:40:48 +0000

    database settings done

--- my_project/urls.py
+++ my_project/urls.py
@@ -22,3 +22,6 @@ urlpatterns = [
     path('',views.index, name="index/"),
     path('showMessage',views.showMessage, name="showMessage")
     ]
+    
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
